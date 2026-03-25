43e9af188a3e74de8fdbe4cbc26891c4eff58274
Juan Esteban Maturana <juanematuranap@gmail.com>
2026-03-25 14:14:36 -0500

    se crea ejercicio3.py

--- listas/ejercicio3.py
+++ listas/ejercicio3.py
@@ -2,4 +2,14 @@ import random
 lista = []
 for i in range (100,200):
     lista.append(random.randint(100, 200))
-print (lista)
+x = lista [0]
+for y in lista:
+    if x< y:
+        x = y
+menor = lista[0]
+for z in lista:
+    if menor > z:
+        menor = z
+m = max(lista)
+menor = min (lista)
+print (f"{lista}. el maximo es {m} y el menor es {menor}")
